2bb4a9e28fe9cf344d5dbedd08549a33c8a56f79
தமிழ்நேரம் <anishprabu.t@gmail.com>
2023-12-18 04:16:00 +0530

    updated

--- பைத்தான்படிவங்கள்/onlinetranslate.py
+++ பைத்தான்படிவங்கள்/onlinetranslate.py
@@ -1,37 +1,28 @@
 import translators as ts
 import polib
+import time
 
 def tr_file(filename):
     po = polib.pofile(filename)
     print("Before : ",po.percent_translated(),"%")
     count = 0
-    errors = ""
-    try:
-        for entry in po.untranslated_entries():
-            try:
-                print(entry.msgid)
-                entry.msgstr=ts.translate_text(entry.msgid, translator='google', from_language = 'en', to_language = 'ta')
-                print(entry.msgstr)
-            except  Exception as e:
-                errors += str(e)
-                count += 1
-            if count == 5:
-                break 
-                   
-    except Exception as e:
-        print("Exception has occured", e)
-        
-    finally :
-        po.save(filename)
-
+    for entry in po.untranslated_entries():
+        try:
+            print(entry.msgid)
+            entry.msgstr=ts.translate_text(entry.msgid, translator='google', from_language = 'en', to_language = 'ta')
+            print(entry.msgstr)
+        except Exception as e:
+            print(e)
+            time.sleep(5*count)
+            count += 1
+        if count == 5:
+            break 
+    po.save(filename)
     # po.save_as_mofile(filename+'.mo')
-
     print("After : ",po.percent_translated(),"%")
-    print("Error count:", count)
-    print("Errors:", errors)
 
 
-files = [ "ta-IN.po",
+files = [ "ta.po",
           ]
 
 for file in files:
