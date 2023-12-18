0075ea03e8bf4e19d99db9949d827ef598d25948
தமிழ்நேரம் <anishprabu.t@gmail.com>
2023-12-18 18:15:38 +0530

    files added

--- பைத்தான்படிவங்கள்/OnlineTranslation.py
+++ பைத்தான்படிவங்கள்/OnlineTranslation.py
@@ -1,39 +1,35 @@
 import translators as ts
 import polib
 import time
+from os import listdir
+from os.path import isfile, join
+import glob
 
 def tr_file(filename):
     po = polib.pofile(filename)
-    print("Before : ",po.percent_translated(),"%")
-    count = 0
-    for entry in po.untranslated_entries():
-        try:
-            print(entry.msgid)
-            entry.msgstr=ts.translate_text(entry.msgid, translator='google', from_language = 'en', to_language = 'ta')
-            print(entry.msgstr)
-        except Exception as e:
-            print(e)
-            time.sleep(5*count)
-            count += 1
-        if count == 5:
-            break 
-    po.save(filename)
-    # po.save_as_mofile(filename+'.mo')
-    print("After : ",po.percent_translated(),"%")
+    pretrans = po.percent_translated()
+    if pretrans != 100:
+        print(filename)
+        print("Before : ",pretrans,"%")
+        count = 0
+        for entry in po.untranslated_entries():
+            try:
+                print(entry.msgid)
+                entry.msgstr=ts.translate_text(entry.msgid, translator='google', from_language = 'en', to_language = 'ta')
+                print(entry.msgstr)
+            except Exception as e:
+                print(e)
+                time.sleep(5*count)
+                count += 1
+            if count == 5:
+                break 
+        po.save(filename)
+        # po.save_as_mofile(filename+'.mo')
+        print("After : ",po.percent_translated(),"%")
 
-
-files = [ "sphinx-1_sphinxcontrib-applehelp-pot_ta.po",
-          "sphinx-1_sphinxcontrib-devhelp-pot_ta.po",
-          "sphinx-1_sphinxcontrib-htmlhelp-pot_ta.po",
-          "sphinx-1_sphinxcontrib-jsmath-pot_ta.po",
-          "sphinx-1_sphinxcontrib-qthelp-pot_ta.po",
-          "sphinx-1_sphinxcontrib-serializinghtml-pot_ta.po",
-          ]
-
+files = glob.glob('./முடிவுபெறாத/*.po')
 for file in files:
-    filepath = "./முடிவுபெறாத/"+file
-    print(filepath)
-    tr_file(filepath)
+    tr_file(file)
 
 
 
