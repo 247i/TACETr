d222dd4b9adc40a6e4e6b4a0f6508118ac305ac3
தமிழ்நேரம் <anishprabu.t@gmail.com>
2024-01-12 21:07:37 +0530

    cleanup

--- பைத்தான்படிவங்கள்/மறுபெயரிடு.py
+++ பைத்தான்படிவங்கள்/மறுபெயரிடு.py
@@ -1,10 +1,11 @@
 import os.path
 import glob
 
-files = glob.glob('.//*.po')
+files = glob.glob('./தானி/openteacher/*.po')
 for file in files:
     folder,filename = os.path.split(file)
-    filename = filename[16:]
+    # filename = filename[24:]
+    filename = filename.replace("translations_org.","",-1)
     newfile = os.path.join(folder,filename)
     print(newfile)
     os.rename(file,newfile)
