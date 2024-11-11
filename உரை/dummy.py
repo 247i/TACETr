3dc5b5f9c5ff825b0ddb34518476218254ab9d01
தமிழ் நேரம் <anishprabu.t@gmail.com>
2024-11-11 05:46:52 +0530

    புதிய

--- உரை/dummy.py
+++ உரை/dummy.py
@@ -1,4 +1,25 @@
 test = "மொழிபெயர்ப்புகளை ஏற்றுகிறது [%கள்]".replace("%கள்", "%s")
-print(test)
+# print(test)
 test = "மொழிபெயர்ப்புகளை ஏற்றுகிறது [%கள்]".find("%s")
-print(test)
+# print(test)
+# %(save_profile) கள்
+# %(save_profile)s
+
+# %(num_contained_saves) டி
+# %(num_contained_saves)d
+
+import re
+ஆங்கிலம் = "%(save_profile)s - English message"
+incom = re.search(r"%\([a-z_A-Z]+\)s", ஆங்கிலம்)
+print(incom)
+
+தமிழ் = "%(தமிழ்) கள் - தமிழ் தகவல்"
+out = re.search(r"%\([a-z_A-Zஂ-௺ ]+\) கள்", தமிழ்)
+print(out)
+
+if out:
+    # re.replace("%(save_profile) கள்", "%(save_profile)s")
+    தமிழ் = தமிழ்.replace(out.match, incom.match)
+    print(தமிழ்)
+    print("exitcode")
+    
