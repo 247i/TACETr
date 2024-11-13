1b084136a48e8169c1e30470d2a3138dbe72259c
தமிழ் நேரம் <anishprabu.t@gmail.com>
2024-11-13 20:56:20 +0530

    Update pota.py

--- உரை/pota.py
+++ உரை/pota.py
@@ -272,13 +272,21 @@ def சரிபார்(மூலம், தமிழ்):
         தமிழ் = தமிழ்.replace("%எல்.எல்.டி.", "%lld")
         தமிழ் = தமிழ்.replace("%LLD", "%lld")
         தமிழ் = தமிழ்.replace("% எல்.எல்.டி.", "%lld")
-    # regular expression based replacements - %(sometext)s
-    அடை_var = re.findall(r"%\([a-z_A-Z]+\)s", மூலம்)
-    அடை_மாறி = re.findall(r"%\([a-z_A-Zஂ-௺]+\)[ ]*[sSகள்ைஎசிலஇன]+", தமிழ்)
 
+    # regular expression based replacements - {{sometext}}
+    அடை_var = re.findall(r"{{[a-z_A-Z]+}}", மூலம்)
+    அடை_மாறி = re.findall(r"{{[a-z_A-Zஂ-௺]+}}", தமிழ்)
     if அடை_மாறி and அடை_var:
         for மாறி, var in zip(அடை_மாறி, அடை_var):
             தமிழ் = தமிழ்.replace(மாறி, var, 1)
+
+    # regular expression based replacements - %(sometext)s
+    அடை_var = re.findall(r"%\([a-z_A-Z]+\)s", மூலம்)
+    அடை_மாறி = re.findall(r"%\([a-z_A-Zஂ-௺]+\)[ ]*[sSகள்ைஎசிலஇன]+", தமிழ்)
+    if அடை_மாறி and அடை_var:
+        for மாறி, var in zip(அடை_மாறி, அடை_var):
+            தமிழ் = தமிழ்.replace(மாறி, var, 1)
+
     # regular expression based replacements - %(sometext)d
     அடை_var = re.findall(r"%\([a-z_A-Z]+\)d", மூலம்)
     அடை_மாறி = re.findall(r"%\([a-z_A-Zஂ-௺ ]+\)[ ]*[dDடி]+", தமிழ்)
