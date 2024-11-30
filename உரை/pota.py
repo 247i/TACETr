b31a5164038bc777e41643422f4688dcc5292e12
தமிழ் நேரம் <anishprabu.t@gmail.com>
2024-12-01 04:46:28 +0530

    Update pota.py

--- உரை/pota.py
+++ உரை/pota.py
@@ -297,8 +297,8 @@ def சரிபார்(மூலம், தமிழ்):
         தமிழ் = தமிழ்.replace("% எல்.எல்.டி.", "%lld")
 
     # regular expression based replacements - {pyvariable}
-    அடை_var = re.findall(r"{[a-z_A-Z]+}", மூலம்)
-    அடை_மாறி = re.findall(r"{[a-z_A-Zஂ-௺]+}", தமிழ்)
+    அடை_var = re.findall(r"{[a-z_A-Z \.:]+}", மூலம்)
+    அடை_மாறி = re.findall(r"{[a-z_A-Z \.:ஂ-௺]+}", தமிழ்)
     if அடை_மாறி and அடை_var:
         for மாறி, var in zip(அடை_மாறி, அடை_var):
             தமிழ் = தமிழ்.replace(மாறி, var, 1)
